bdd5afaac3aab427546665fb54a7c783ba8bbb51
wesam abu shaqra <80597414+wesamalaliwa@users.noreply.github.com>
2022-12-29 00:29:24 +0300

    Add files via upload

---
+++ task2.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="task2.aspx.cs" Inherits="ASP.NET_TASKS.task2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                 <div>
+           
+            <div class="input-group mb-3">
+                <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+           
+<%--                <asp:Button ID="Button1" runat="server" OnClick="ccc"  Text="Button" />--%>
+                <asp:Button ID="Button2" runat="server" Text="Button" OnClick="Button2_Click" />
+</div>
+        </div>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ task2.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace ASP.NET_TASKS
+{
+    public partial class task2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ccc(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection =
+           new SqlConnection("data source =  DESKTOP-N9T9K9N\\SQLEXPRESS; database = tasks ; integrated security=SSPI");
+            SqlCommand command = new SqlCommand($"insert into tasks (comment) values (@commment) ", connection);
+            command.Parameters.AddWithValue("@commment", TextBox1.Text);
+            connection.Open();
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            ScriptManager.RegisterClientScriptBlock(this.Page, this.Page.GetType(), "alert", "alert('Member Registered Sucessfully');", true);
+
+        }
+    }
+}
